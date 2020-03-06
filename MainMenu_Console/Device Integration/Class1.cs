dff7801ec6609fa43cdcf2185deb920107e7d23e
colemank0259 <43657783+colemank0259@users.noreply.github.com>
2020-03-06 11:10:04 -0500

    Added Device Integration Class Library

---
+++ MainMenu_Console/Device Integration/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Device_Integration
+{
+    public class Class1
+    {
+    }
+}
